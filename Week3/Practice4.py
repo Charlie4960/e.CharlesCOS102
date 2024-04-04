f30ece35618a1ab91e946dd9cb3569591c900a40
Charlie4960 <charlesedozie496@gmail.com>
2024-04-04 02:30:35 +0100

    My Second Commit

---
+++ Week3/Practice4.py
@@ -0,0 +1,18 @@
+tuple = ("Ekiti", 750, 'Oshogbo', 250, "Akure", 500)
+s_tuple = ("Abeokuta", 300, "Ogbomoso")
+# Prints the complete tuple
+print (tuple)
+#PRints last element of the tuple 
+Print (tuple[-1])
+
+#PRints elements of the tuple starting from 2nd till 3rd
+print(tuuple[2:4])
+
+#Prints elements of the tuple starting from 3rd element
+print(tuple[3:1])
+
+#Prints the contents of the tuple twice
+print (s_tuple * 3)
+
+#prints concatenated tuples
+print (tuple + s_tuple)
---
+++ Week3/Practice5.py
@@ -0,0 +1,18 @@
+# Returns false as game_1 is not equal to game_2
+game_1 = 2
+game_2 = 4
+print(bool(game_1 == game_2))
+#or
+print(game_1 == game _2)
+
+#Return false as val is None
+val = None
+print(bool(val))
+
+#returns falses as num is an empty sequence
+num = ()
+print(bool(num))
+
+#returns true as age is boolean
+age = True
+print(bool(age))
